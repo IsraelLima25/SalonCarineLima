bb7ace7ab795e4ae44163c947a4e7b78ce8345cc
Israel Lima <israel_lima_16@hotmail.com>
2020-08-30 00:17:39 -0300

    Estrutura JPA Configurada

--- src/main/java/br/com/salon/carine/lima/conf/AppWebConfiguration.java
+++ src/main/java/br/com/salon/carine/lima/conf/AppWebConfiguration.java
@@ -7,7 +7,7 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
-import br.com.salon.carine.lima.controller.HomeController;
+import br.com.salon.carine.lima.controllers.HomeController;
 
 @EnableWebMvc
 @ComponentScan(basePackageClasses = HomeController.class)
--- src/main/java/br/com/salon/carine/lima/controllers/HomeController.java
+++ src/main/java/br/com/salon/carine/lima/controllers/HomeController.java
@@ -1,4 +1,4 @@
-package br.com.salon.carine.lima.controller;
+package br.com.salon.carine.lima.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
