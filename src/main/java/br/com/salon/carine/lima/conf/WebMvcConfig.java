677e1177ed3950d0ef3c9411462c709dca66b8e2
Israel Lima <israel_lima_16@hotmail.com>
2020-11-14 01:34:37 -0300

    imports

--- src/main/java/br/com/salon/carine/lima/conf/WebMvcConfig.java
+++ src/main/java/br/com/salon/carine/lima/conf/WebMvcConfig.java
@@ -1,5 +1,8 @@
 package br.com.salon.carine.lima.conf;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 import org.springframework.cache.CacheManager;
@@ -8,9 +11,14 @@ import org.springframework.cache.guava.GuavaCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.web.accept.ContentNegotiationManager;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.ContentNegotiatingViewResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 import com.google.common.cache.CacheBuilder;
@@ -43,7 +51,40 @@ public class WebMvcConfig implements WebMvcConfigurer {
 		manager.setCacheBuilder(builder);
 		return manager;
 	}
+	
+	@Bean
+	public ViewResolver contenNegotiationViewResolver(ContentNegotiationManager manager) {
+		
+		List<ViewResolver> viewResolvers = new ArrayList<>();
+		viewResolvers.add(resolver());
+		viewResolvers.add(new JsonViewResolver());
+		
+		ContentNegotiatingViewResolver resolver = new ContentNegotiatingViewResolver();
+		resolver.setViewResolvers(viewResolvers);
+		resolver.setContentNegotiationManager(manager);
 
+		return resolver;
+		
+	}
+
+	@Bean
+	public JavaMailSender getJavaMailSender() {
+	    JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+	    mailSender.setHost("smtp.gmail.com");
+	    mailSender.setPort(587);
+	    
+	    mailSender.setUsername("sagsoftwareagendamentos@gmail.com");
+	    mailSender.setPassword("");
+	    
+	    Properties props = mailSender.getJavaMailProperties();
+	    props.put("mail.transport.protocol", "smtp");
+	    props.put("mail.smtp.auth", "true");
+	    props.put("mail.smtp.starttls.enable", "true");
+	    props.put("mail.debug", "true");
+	    
+	    return mailSender;
+	}
+	
 //    public void addResourceHandlers(ResourceHandlerRegistry registry) {
 //        registry
 //            .addResourceHandler("/resources/**")
